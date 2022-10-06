5ff1f098fbfc7b46800adc925d9aee67bbed0367
Dax Tangco <daxtangco@gmail.com>
2022-10-06 23:35:38 +0800

    changed links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://daxtangco.github.io/portfolio'
+      href='https://portfolio-daxtangco.vercel.app/'
       className='link footer__link'
     >
       Created By Dax Tangco
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://daxtangco.github.io/portfolio',
+  homepage: 'https://portfolio-daxtangco.vercel.app/',
   title: 'DT.',
 }
 
